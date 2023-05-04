1b1fc2d492228ae60d3d369de0d5e828b4bb0230
wyh68l <1074021399@qq.com>
2023-05-05 07:00:42 +0800

    fix：优化header代码

--- apps/chat/src/app/layout.tsx
+++ apps/chat/src/app/layout.tsx
@@ -29,32 +29,37 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;700;900&display=swap"
           rel="stylesheet"
         ></link>
-          <script type="text/javascript">
+          <script
+              type="text/javascript"
+              dangerouslySetInnerHTML={{
+                  __html: `
               (function() {
-              window.sib = {
+                window.sib = {
                   equeue: [],
                   client_key: "jrh4yiitxzni705lzvk1z2yq"
-              };
-              /* OPTIONAL: email for identify request*/
-              // window.sib.email_id = 'example@domain.com';
-              window.sendinblue = {};
-              for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
-              (function(k) {
-              window.sendinblue[k] = function() {
-              var arg = Array.prototype.slice.call(arguments);
-              (window.sib[k] || function() {
-              var t = {};
-              t[k] = arg;
-              window.sib.equeue.push(t);
-          })(arg[0], arg[1], arg[2], arg[3]);
-          };
-          })(j[i]);
-          }
-              var n = document.createElement("script"),
-              i = document.getElementsByTagName("script")[0];
-              n.type = "text/javascript", n.id = "sendinblue-js", n.async = !0, n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key, i.parentNode.insertBefore(n, i), window.sendinblue.page();
-          })();
-          </script>
+                };
+                /* OPTIONAL: email for identify request*/
+                // window.sib.email_id = 'example@domain.com';
+                window.sendinblue = {};
+                for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
+                  (function(k) {
+                    window.sendinblue[k] = function() {
+                      var arg = Array.prototype.slice.call(arguments);
+                      (window.sib[k] || function() {
+                        var t = {};
+                        t[k] = arg;
+                        window.sib.equeue.push(t);
+                      })(arg[0], arg[1], arg[2], arg[3]);
+                    };
+                  })(j[i]);
+                }
+                var n = document.createElement("script"),
+                  i = document.getElementsByTagName("script")[0];
+                n.type = "text/javascript", n.id = "sendinblue-js", n.async = !0, n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key, i.parentNode.insertBefore(n, i), window.sendinblue.page();
+              })();
+            `
+              }}
+          />
       </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
